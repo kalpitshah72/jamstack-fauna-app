86eaf74748f6693b06e3118ffc6b8fc7e514fc00
Kalpit <kalpits21@gmail.com>
2020-09-23 17:29:54 +0530

    create frontend using react with bootstrap used fetch in the frontend and axios in the backend

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from "react";
+import LinkForm from "./components/LinkForm";
+import LinkList from "./components/LinkList";
+
+function App() {
+  const [links, setLinks] = useState([]);
+  const loadLinks = async () => {
+    try {
+      const res = await fetch("/.netlify/functions/getLinks");
+      const links = await res.json();
+      setLinks(links);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    loadLinks();
+  }, []);
+
+  return (
+    <div className="container py-5">
+      <h1 className="text-center mb-5">List O' Link</h1>
+      <LinkForm refreshLinks={loadLinks} />
+      <LinkList links={links} refreshLinks={loadLinks} />
+    </div>
+  );
+}
+
+export default App;
